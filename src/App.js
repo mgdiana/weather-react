d7aeba945ba4de34b91426be61fa9f44fa60cea2
mgdiana <49156411+mgdiana@users.noreply.github.com>
2022-07-17 19:53:32 +0200

    Weather js Created

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from React</h1>
+        <h1>Weather App</h1>
         <Weather />
+        <div class="container">
+          <div id="weather" class="weather-container"></div>
+          <small>
+            <a
+              href="https://github.com/wecodeschool/react-weather"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            , by Maria Giovanna Diana hosted on
+            <a href="https://shecodes.io/"> Netlify</a>
+          </small>
+        </div>
       </header>
     </div>
   );
